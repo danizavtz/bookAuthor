f1a09e8128de0ca79b6d161dea66bc100333e16a
Danizavtz <daninove@gmail.com>
2020-06-27 16:31:30 -0300

    Atualizacao das versoes das libs

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express');
 const logger = require('morgan');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const expressValidator = require('express-validator');
 
